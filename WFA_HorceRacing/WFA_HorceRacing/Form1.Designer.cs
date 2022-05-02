80f4131454c21ab146389d41839211a7c4c27ad0
Doruk <drktny@gmail.com>
2022-05-02 19:37:22 +0300

    UI is created

--- WFA_HorceRacing/WFA_HorceRacing/Form1.Designer.cs
+++ WFA_HorceRacing/WFA_HorceRacing/Form1.Designer.cs
@@ -29,20 +29,284 @@ namespace WFA_HorceRacing
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblCommentator = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBet3 = new System.Windows.Forms.Button();
+            this.nmr3 = new System.Windows.Forms.NumericUpDown();
+            this.txtOddRatio3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBet2 = new System.Windows.Forms.Button();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.pbHorse3 = new System.Windows.Forms.PictureBox();
+            this.nmr2 = new System.Windows.Forms.NumericUpDown();
+            this.txtOddRatio2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBet1 = new System.Windows.Forms.Button();
+            this.nmr1 = new System.Windows.Forms.NumericUpDown();
+            this.txtOddRatio1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbHorse2 = new System.Windows.Forms.PictureBox();
+            this.pbHorse1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHorse3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHorse2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHorse1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblCommentator
+            // 
+            this.lblCommentator.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCommentator.Location = new System.Drawing.Point(937, 452);
+            this.lblCommentator.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCommentator.Name = "lblCommentator";
+            this.lblCommentator.Size = new System.Drawing.Size(287, 55);
+            this.lblCommentator.TabIndex = 13;
+            this.lblCommentator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(25, 497);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(140, 50);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnBet3
+            // 
+            this.btnBet3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBet3.Location = new System.Drawing.Point(600, 71);
+            this.btnBet3.Name = "btnBet3";
+            this.btnBet3.Size = new System.Drawing.Size(61, 32);
+            this.btnBet3.TabIndex = 11;
+            this.btnBet3.Text = "Bet";
+            this.btnBet3.UseVisualStyleBackColor = true;
+            // 
+            // nmr3
+            // 
+            this.nmr3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nmr3.Location = new System.Drawing.Point(587, 39);
+            this.nmr3.Name = "nmr3";
+            this.nmr3.Size = new System.Drawing.Size(86, 26);
+            this.nmr3.TabIndex = 10;
+            // 
+            // txtOddRatio3
+            // 
+            this.txtOddRatio3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOddRatio3.Location = new System.Drawing.Point(587, 7);
+            this.txtOddRatio3.Name = "txtOddRatio3";
+            this.txtOddRatio3.Size = new System.Drawing.Size(86, 26);
+            this.txtOddRatio3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(498, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "3rdHorse:";
+            // 
+            // btnBet2
+            // 
+            this.btnBet2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBet2.Location = new System.Drawing.Point(406, 71);
+            this.btnBet2.Name = "btnBet2";
+            this.btnBet2.Size = new System.Drawing.Size(61, 32);
+            this.btnBet2.TabIndex = 7;
+            this.btnBet2.Text = "Bet";
+            this.btnBet2.UseVisualStyleBackColor = true;
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.BackColor = System.Drawing.Color.DarkRed;
+            this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFinish.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblFinish.Location = new System.Drawing.Point(1181, 89);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(45, 321);
+            this.lblFinish.TabIndex = 10;
+            this.lblFinish.Text = "FINISH";
+            this.lblFinish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbHorse3
+            // 
+            this.pbHorse3.Image = ((System.Drawing.Image)(resources.GetObject("pbHorse3.Image")));
+            this.pbHorse3.Location = new System.Drawing.Point(8, 330);
+            this.pbHorse3.Name = "pbHorse3";
+            this.pbHorse3.Size = new System.Drawing.Size(50, 50);
+            this.pbHorse3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbHorse3.TabIndex = 6;
+            this.pbHorse3.TabStop = false;
+            // 
+            // nmr2
+            // 
+            this.nmr2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nmr2.Location = new System.Drawing.Point(393, 39);
+            this.nmr2.Name = "nmr2";
+            this.nmr2.Size = new System.Drawing.Size(86, 26);
+            this.nmr2.TabIndex = 6;
+            // 
+            // txtOddRatio2
+            // 
+            this.txtOddRatio2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOddRatio2.Location = new System.Drawing.Point(393, 7);
+            this.txtOddRatio2.Name = "txtOddRatio2";
+            this.txtOddRatio2.Size = new System.Drawing.Size(86, 26);
+            this.txtOddRatio2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(304, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "2ndHorse:";
+            // 
+            // btnBet1
+            // 
+            this.btnBet1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBet1.Location = new System.Drawing.Point(187, 71);
+            this.btnBet1.Name = "btnBet1";
+            this.btnBet1.Size = new System.Drawing.Size(61, 32);
+            this.btnBet1.TabIndex = 3;
+            this.btnBet1.Text = "Bet";
+            this.btnBet1.UseVisualStyleBackColor = true;
+            // 
+            // nmr1
+            // 
+            this.nmr1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nmr1.Location = new System.Drawing.Point(174, 39);
+            this.nmr1.Name = "nmr1";
+            this.nmr1.Size = new System.Drawing.Size(86, 26);
+            this.nmr1.TabIndex = 2;
+            // 
+            // txtOddRatio1
+            // 
+            this.txtOddRatio1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOddRatio1.Location = new System.Drawing.Point(174, 7);
+            this.txtOddRatio1.Name = "txtOddRatio1";
+            this.txtOddRatio1.Size = new System.Drawing.Size(86, 26);
+            this.txtOddRatio1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(85, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "1stHorse:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBet3);
+            this.groupBox1.Controls.Add(this.nmr3);
+            this.groupBox1.Controls.Add(this.txtOddRatio3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnBet2);
+            this.groupBox1.Controls.Add(this.nmr2);
+            this.groupBox1.Controls.Add(this.txtOddRatio2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnBet1);
+            this.groupBox1.Controls.Add(this.nmr1);
+            this.groupBox1.Controls.Add(this.txtOddRatio1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(219, 428);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(711, 127);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bet Screen";
+            // 
+            // pbHorse2
+            // 
+            this.pbHorse2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbHorse2.BackgroundImage")));
+            this.pbHorse2.Image = ((System.Drawing.Image)(resources.GetObject("pbHorse2.Image")));
+            this.pbHorse2.Location = new System.Drawing.Point(8, 230);
+            this.pbHorse2.Name = "pbHorse2";
+            this.pbHorse2.Size = new System.Drawing.Size(50, 50);
+            this.pbHorse2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbHorse2.TabIndex = 7;
+            this.pbHorse2.TabStop = false;
+            // 
+            // pbHorse1
+            // 
+            this.pbHorse1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbHorse1.BackgroundImage")));
+            this.pbHorse1.Image = ((System.Drawing.Image)(resources.GetObject("pbHorse1.Image")));
+            this.pbHorse1.Location = new System.Drawing.Point(8, 130);
+            this.pbHorse1.Name = "pbHorse1";
+            this.pbHorse1.Size = new System.Drawing.Size(50, 50);
+            this.pbHorse1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbHorse1.TabIndex = 8;
+            this.pbHorse1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1185, 722);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1338, 556);
+            this.Controls.Add(this.lblCommentator);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.pbHorse3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pbHorse2);
+            this.Controls.Add(this.pbHorse1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nmr3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHorse3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHorse2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHorse1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCommentator;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBet3;
+        private System.Windows.Forms.NumericUpDown nmr3;
+        private System.Windows.Forms.TextBox txtOddRatio3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBet2;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.PictureBox pbHorse3;
+        private System.Windows.Forms.NumericUpDown nmr2;
+        private System.Windows.Forms.TextBox txtOddRatio2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBet1;
+        private System.Windows.Forms.NumericUpDown nmr1;
+        private System.Windows.Forms.TextBox txtOddRatio1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbHorse2;
+        private System.Windows.Forms.PictureBox pbHorse1;
     }
 }
 
